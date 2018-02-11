738d07578fc1284556ba18493ab3af4d847074f7
jiro.kirameki <yuhosatoh@gmail.com>
2018-02-11 10:28:44 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,21 @@
+class TasksController < ApplicationController
+end
+
+
+
+def index
+    @tasks = Task.all
+end
+
+def show
+    @task = Task.find(params[:id])
+end
+
+def new
+    @task = Task.new
+end
+
+ def edit
+    @task = Task.find(params[:id])
+ end
+ 
