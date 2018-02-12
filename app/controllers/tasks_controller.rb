c72805c15a124b3b1a447ad81ba650027f61686c
jiro.kirameki <yuhosatoh@gmail.com>
2018-02-12 13:12:50 +0000

    CRUD messages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,6 +11,34 @@ class TasksController < ApplicationController
  def new
     @task = Task.new
  end
+ 
+ def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
+ end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
 
  def edit
     @task = Task.find(params[:id])
