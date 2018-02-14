7ef16b4384d05e805d5d71bdcfe889a246690385
jiro.kirameki <yuhosatoh@gmail.com>
2018-02-14 10:22:09 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
  def index
     @tasks = Task.all
@@ -49,6 +50,16 @@ class TasksController < ApplicationController
  end
  
    # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
+  
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
     params.require(:task).permit(:content)
   end
